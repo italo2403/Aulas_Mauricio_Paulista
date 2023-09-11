0584042820e169511d4f37da17d89b450e4b175d
Ítalo Nunes <37880520+italo2403@users.noreply.github.com>
2023-09-11 10:58:47 -0300

    Add files via upload

---
+++ Aula01/assets/js/script.js
@@ -0,0 +1,37 @@
+function Enviar() {
+  let id = document.querySelector("#login").value;
+  let senha = document.querySelector("#senha").value;
+
+if (id === "Italo" || senha === 123) {
+   alert("Bem Vindo")
+} else {
+   alert("perdeu!")
+}
+
+ }
+
+// let nome = prompt("Entre com seu nome");
+// document.write(nome);
+//  console.log(nome);
+//   alert(nome);
+
+// Comentário de Linha
+/**
+ * Posso passar várias informações aqui
+ * let e var
+ * 
+ * let = variável de bloco 
+ * var = variável de escopo
+ * 
+ * Comando de Saída
+ * alert
+ * document.write
+ * console.log
+ * 
+ * Comando de Entrada
+ * prompt
+ * document
+ * 
+ * 
+ * 
+ */
